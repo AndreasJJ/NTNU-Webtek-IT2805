1c57a2105b31e2acc56926365d42863879ca95d1
Andreas <andreasjj@gmail.com>
2017-10-27 16:17:36 +0200

    A madda fucking revamp of the frontpage
    Things are fucked, it's kinda fixed now. The newsbar still has to be fixed.

--- Nettside/js/navigation.js
+++ Nettside/js/navigation.js
@@ -1,27 +1,22 @@
 function openSidebarMenu() {
-	var sidebar = document.getElementById('sidebar');
-	var mainPanel = document.getElementById('mainPanel');
-	if(window.getComputedStyle(sidebar, null).width != "0%") {
-		sidebar.style.width = '0%';
-		sidebar.style.display = 'none';
-		mainPanel.style.width = '100%';
+	var sideNav = document.getElementById('sideNav');
+	var newsBar = document.getElementById('nyheter');
+	var mainContent = document.getElementById('mainContent');
+	if(window.getComputedStyle(sideNav, null).width != "0%" & sideNav.style.display != 'none') {
+		sideNav.style.display = 'none';
+		newsBar.style.display = 'block';
+		mainContent.style.display = 'block';
+		mainContent.style.width = '80%';
 	} else if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-		sidebar.style.display = 'block';
-		sidebar.style.width = '100%';
-		mainPanel.style.width = '0%';
+		sideNav.style.display = 'block';
+		sideNav.style.width = '100%';
+		mainContent.style.display = 'none';
+		newsBar.style.display = 'none';
 	}
 	else {
-		sidebar.style.display = 'block';
-		//sidebar.animate([
-		  // keyframes
-		//  { transform: 'translateX(-300px)' }, 
-		//  { transform: 'translateX(0px)' }
-		//], { 
-		  // timing options
-		//  duration: 500,
-		//  iterations: 1
-		//});
-		sidebar.style.width = 'calc(100%/3)';
-		mainPanel.style.width = 'calc((100%/3)*2)';
+		sideNav.style.display = 'block';
+		sideNav.style.width = 'calc(100%/3)';
+		mainContent.style.width = 'calc((100%/3)*2)';
+		newsBar.style.display = 'none';
 	}
 }
