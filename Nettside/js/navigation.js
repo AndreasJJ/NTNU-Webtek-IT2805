c882bcee46ea57cf5a8ba1dee09e403c5a514da3
Andreas <andreasjj@gmail.com>
2017-11-05 21:22:52 +0100

    SideNav update
    removed the links and added onclick eventlistener instead to the li elements to make the whole section clickable

--- Nettside/js/navigation.js
+++ Nettside/js/navigation.js
@@ -1,8 +1,43 @@
+var sideNavLinks = document.getElementsByClassName("sideNavLink");
+Array.from(sideNavLinks).forEach(function(element) {
+      element.addEventListener('click', linkNav);
+});
+
+
+function linkNav(e) {
+	switch(e.target.innerText.toLowerCase()) {
+		case "om oss":
+			location.href = 'om.html';
+			break;
+		case "program":
+			location.href = 'program.html';
+			break;
+		case "støtt oss":
+			location.href = 'stotte.html';
+			break;
+		case "kontakt oss":
+			location.href = 'kontakt.html';
+			break;
+		case "historie":
+			location.href = 'historie.html';
+			break;
+		case "politikere":
+			location.href = 'politikere.html';
+			break;
+	}
+}
+
 function openSidebarMenu() {
 	var sideNav = document.getElementById('sideNav');
 	var newsBar = document.getElementById('nyheter');
 	var mainContent = document.getElementById('mainContent');
-	if(window.getComputedStyle(sideNav, null).width != "0%" & sideNav.style.display != 'none') {
+	if(window.getComputedStyle(sideNav, null).width != "0%" & sideNav.style.display != 'none' & /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		sideNav.style.display = 'none';
+		newsBar.style.display = 'none';
+		mainContent.style.display = 'block';
+		mainContent.style.width = '100%';
+	}
+	else if(window.getComputedStyle(sideNav, null).width != "0%" & sideNav.style.display != 'none') {
 		sideNav.style.display = 'none';
 		newsBar.style.display = 'block';
 		mainContent.style.display = 'block';
@@ -15,8 +50,8 @@ function openSidebarMenu() {
 	}
 	else {
 		sideNav.style.display = 'block';
-		sideNav.style.width = 'calc(100%/3)';
-		mainContent.style.width = 'calc((100%/3)*2)';
+		sideNav.style.width = 'calc(100%/5)';
+		mainContent.style.width = 'calc((100%/5)*4)';
 		newsBar.style.display = 'none';
 	}
 }
