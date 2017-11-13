68e842c5480498f68b020030404f4d3d86543236
Andreas <andreasjj@gmail.com>
2017-11-13 17:31:04 +0100

    Navigation.js comments added

--- Nettside/js/navigation.js
+++ Nettside/js/navigation.js
@@ -1,9 +1,10 @@
+/* Henter alle elementene med klasse navnet sideNavLink og har en loop som gir alle en eventlistener */
 var sideNavLinks = document.getElementsByClassName("sideNavLink");
 Array.from(sideNavLinks).forEach( function(element) {
       element.addEventListener('click', linkNav);
 });
 
-
+/* En funksjon som gjør alle li elementene i ul listen i side navigasjonsbaren til lenker og denne funksjonen forteller dem hvor de skal linke. */
 function linkNav(e) {
 	switch (e.target.innerText.toLowerCase()) {
 		case "om oss":
@@ -32,25 +33,31 @@ function openSidebarMenu() {
 	var hostname = "http://" + window.location.hostname + "/";
 	var sideNav = document.getElementById('sideNav');
 	var newsBar = document.getElementById('nyheter');
+	/* Sjekker om det er index siden eller en annen side */
 	if (url == hostname) {
 		var mainContent = document.getElementById('mainContent');
+		/* Sjekker om sideNav er åpen og om det er en mobil */
 		if(window.getComputedStyle(sideNav, null).display != 'none' & /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 			sideNav.style.display = 'none';
 			newsBar.style.display = 'none';
 			mainContent.style.display = 'block';
 			mainContent.style.width = '100%';
 		}
+		/* Sjekker om sideNAv er åpen */
 		else if(window.getComputedStyle(sideNav, null).display != 'none') {
 			sideNav.style.display = 'none';
 			newsBar.style.display = 'block';
 			mainContent.style.display = 'block';
 			mainContent.style.width = '80%';
-		} else if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		} 
+		/* hvis sidenav er lukket og det er en mobil */
+		else if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 			sideNav.style.display = 'block';
 			sideNav.style.width = '100%';
 			mainContent.style.display = 'none';
 			newsBar.style.display = 'none';
 		}
+		/* hvis sidenav er lukket og det ikke er en mobil */
 		else {
 			sideNav.style.display = 'block';
 			mainContent.style.width = 'calc((100%/5)*4)';
@@ -58,20 +65,25 @@ function openSidebarMenu() {
 		}
 	} else {
 		var mainContent = document.getElementById('mainContent');
+		/* Sjekker om sideNav er åpen og om det er en mobil */
 		if(window.getComputedStyle(sideNav, null).display != 'none' & /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 			sideNav.style.display = 'none';
 			mainContent.style.display = 'block';
 			mainContent.style.width = '100%';
 		}
+		/* Sjekker om sideNAv er åpen */
 		else if(window.getComputedStyle(sideNav, null).display != 'none') {
 			sideNav.style.display = 'none';
 			mainContent.style.display = 'block';
 			mainContent.style.width = '100%';
-		} else if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		} 
+		/* hvis sidenav er lukket og det er en mobil */
+		else if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 			sideNav.style.display = 'block';
 			sideNav.style.width = '100%';
 			mainContent.style.display = 'none';
 		}
+		/* hvis sidenav er lukket og det ikke er en mobil */
 		else {
 			sideNav.style.display = 'block';
 			mainContent.style.width = 'calc((100%/5)*4)';
